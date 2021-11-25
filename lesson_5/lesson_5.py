06bc5c4638e445dfc2c162b29965ce89ded7ab9e
Sergey <uldman.sergey@gmail.com>
2021-11-25 19:06:00 +0200

    Add lesson 4 task-4

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -66,4 +66,39 @@ while five_words < 5:  # повторяем для 5 слов
     five_words += 1
     print('Новое слово  = ', new_word)
 
+#Task 4
+#The math quiz program Write a program that asks the answer for a mathematical expression,
+# checks whether the user is right or wrong, and then responds with a message accordingly.
+print('\nTask 4')
 
+print('Математическая викторина')
+guesses = 3
+count_question = 1
+question_1 = """Вопрос №1
+Решите пример:  1.36 + 2.8 * 0.7 = """
+question_2 = """Вопрос №2
+Чему равен х?   5 * (25 + x) = 1740 + 25 = """
+question_3 = """Вопрос №2
+Чему равен х?   5 * (25 + x) = 1740 + 25 = """
+
+while guesses > 0 and count_question < 3:
+    if count_question == 1 and guesses > 0:
+        guess = input(question_1)
+        if  guess == '3.32':
+            print('Правильно!')
+            count_question += 1
+        else:
+            print('Неправильно!')
+            guesses -= 1
+
+    elif count_question == 2 and guesses > 0:
+        guess = input(question_2)
+        if  guess == '328':
+            print('Правильно!')
+            print('Поздравляем, вы выиграли!!!')
+            count_question += 1
+        else:
+            print('Неправильно!')
+            guesses -= 1
+    if guesses == 0:
+        print('Вы проиграли, у вас закончились  попытки')
