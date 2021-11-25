30478ac1ea526e1b797c8f0cf94cec6b223b5976
Sergey <uldman.sergey@gmail.com>
2021-11-25 05:46:56 +0200

    Add lesson 4 task-3

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -2,16 +2,16 @@
 #The Guessing Game.
 #Write a program that generates a random number between 1 and 10 and lets the user guess what number was generated.
 # The result should be sent back to the user via a print statement.
+print('\nTask 1')
 
 import random
 guesses = 3
 random_number = random.randint(1, 10)
-print(random_number)
 print('Введите число от 1 до 10\n У вас 3 попытки')
 while guesses > 0:
     player_guess = input()
 
-    if player_guess.isdigit() and guesses != 0:
+    if player_guess.isdigit() and guesses != 0:  # Проверка на валидность ввода
         guesses -= 1
         player_guess = int(player_guess)
         if player_guess == random_number:
@@ -24,4 +24,30 @@ while guesses > 0:
 
     else:
         print('Неправильный ввод. Пожалуйста введите число от 1 до 10')
+
+#Task 2
+#The birthday greeting program.
+#Write a program that takes your name as input, and then your age as input and greets you with the following:
+#“Hello <name>, on your next birthday you’ll be <age+1> years”
+print('\nTask 2')
+
+name = input('Type your name   ')
+age = input('Type yore age   ')
+if age.isdigit():
+
+    print(f'Hello {name}, on your next birthday you’ll be {int(age) + 1} years')
+else:
+    print('Invalid type')
+
+#Task 3
+
+#Create a program that reads an input string and then creates and prints 5 random
+# strings from characters of the input string.
+#For example, the program obtained the word ‘hello’, so it should print 5 random strings(words)
+# that combine characters ‘h’, ‘e’, ‘l’, ‘l’, ‘o’ -> ‘hlelo’, ‘olelh’, ‘loleh’ …
+
+print('\nTask 3')
+
+import random
+input_string = input('Введите строку ')
 
