7b77fb480c20a43618863f84e8dfbc3521fec462
Sergey <uldman.sergey@gmail.com>
2021-12-14 19:38:28 +0200

    Add lesson 11 Task 4

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -109,3 +109,26 @@ if __name__ == '__main__':
         elif user_type == 'exit':
             break
 
+# Task 4
+# Создайте класс Friend для хранения имени name и телефона phone.
+# Обратите внимание, у друга может не быть телефона.
+
+if __name__ == '__main__':
+    class Friend:
+        def __init__(self, name, phone=''):
+            self.name = name
+            self.phone = phone
+
+
+    u1 = Friend("Андрей", "+380331233333")
+    u2 = Friend("Петр", )
+
+    assert hasattr(u1, "name")
+    assert hasattr(u1, "phone")
+    assert u1.name == "Андрей"
+    assert u2.name == "Петр"
+    assert u1.phone == "+380331233333"
+    assert u2.phone == ""
+
+
+
