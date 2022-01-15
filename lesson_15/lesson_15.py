1681af681c17eb18da26a44b0b7a096576344072
Sergey <uldman.sergey@gmail.com>
2022-01-15 16:53:39 +0200

    Add lesson 15 Task 3

--- lesson_15/lesson_15.py
+++ lesson_15/lesson_15.py
@@ -45,3 +45,43 @@ if __name__ == '__main__':
         return f"{name} drinks pepsi in his brand new BMW!"
 
     assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
+
+# Task 3
+# Write a decorator `arg_rules` that validates arguments passed to the function.
+# A decorator should take 3 arguments:
+# max_length: 15
+# type_: str
+# contains: [] - list of symbols that an argument should contain
+# If some of the rules' checks returns False, the function should return False and print the reason it failed;
+# otherwise, return the result.
+
+if __name__ == '__main__':
+    def arg_rules(type_: type, max_length: int, contains: list):
+        def decorator(func):
+            def inner(*args, **kwargs):
+                if isinstance(*args, type_):
+                    if len(*args) < max_length:
+                        string_name = args[0]
+                        for val in contains:
+                            if string_name.find(val) > -1:
+                                return func(*args,**kwargs)
+                        print(f'Передаваемая строка не содержит значения {contains}')
+                        return False
+                    else:
+                        print(f'Длина строки превышает допустимое значение {max_length}')
+                        return False
+                else:
+                    print(f'Неподходящий тип, ожидаемы тип {type_}')
+                    return False
+            return inner
+        return decorator
+
+
+    @arg_rules(type_=str, max_length=15, contains=['05', '@'])
+    def create_slogan(name: str) -> str:
+        return f"{name} drinks pepsi in his brand new BMW!"
+
+
+    assert create_slogan('johndoe05@gmail.com') is False
+
+    assert create_slogan('S@SH05') == 'S@SH05 drinks pepsi in his brand new BMW!'
