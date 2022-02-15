be28c9731dd0a283c02afc6387ff2a62809665ad
Sergey <uldman.sergey@gmail.com>
2022-02-16 00:13:29 +0200

    Add lesson 25 Task 2,3,4

--- lesson_25/lesson_25.py
+++ lesson_25/lesson_25.py
@@ -36,3 +36,99 @@ if __name__ == '__main__':
 
 
     # Task 2
+    # from typing import Optional
+    #
+    #
+    # def is_palindrome(looking_str: str, index: int = 0) -> bool:
+    #     """
+    #     Checks if input string is Palindrome
+    #     >>> is_palindrome('mom')
+    #     True
+    #
+    #     >>> is_palindrome('sassas')
+    #     True
+    #
+    #     >>> is_palindrome('o')
+    #     True
+    #     """
+    #     pass
+
+    from typing import Optional
+
+
+    def is_palindrome(looking_str: str, index: int = 0) -> bool:
+        if index > len(looking_str) or len(looking_str) == 1:
+            return True
+        elif not looking_str:
+            return False
+        elif looking_str[index] == looking_str[-1 - index] and index < len(looking_str) / 2:
+            index += 1
+            is_palindrome(looking_str, index)
+            return True
+        else:
+            return False
+
+
+    assert is_palindrome('mom')
+    assert is_palindrome('sassas')
+    assert is_palindrome('o')
+    assert is_palindrome('') == False
+
+
+    # Task 3
+
+
+    # from typing import Optional
+    # def mult(a: int, n: int) -> int:
+    #     """
+    #     This function works only with positive integers
+    #
+    #     >>> mult(2, 4) == 8
+    #     True
+    #
+    #     >>> mult(2, 0) == 0
+    #     True
+    #
+    #     >>> mult(2, -4)
+    #     ValueError("This function works only with postive integers")
+    #     """
+    from typing import Optional
+
+
+    def mult(a: int, n: int) -> int:
+        if n < 0:
+            raise ValueError('This function works only with postive integers')
+        if n == 0:
+            return 0
+        if n == 1:
+            return a
+        return a + mult(a, n - 1)
+
+
+    assert mult(2, 4) == 8
+    assert mult(2, 0) == 0
+    # mult(2, -4)
+
+    #  Task 4
+    # def reverse(input_str: str) -> str:
+    #     """
+    #     Function returns reversed input string
+    #     >>> reverse("hello") == "olleh"
+    #     True
+    #     >>> reverse("o") == "o"
+    #     True
+    #     """
+
+    def reverse(input_str: str) -> str:
+        if not input_str:
+            raise ValueError('Пустая строка')
+        if len(input_str) == 1:
+            return input_str[0]
+
+        return input_str[-1] + reverse(input_str[:-1])
+
+
+    assert reverse('hello') == 'olleh'
+    assert reverse('o') == 'o'
+
+
