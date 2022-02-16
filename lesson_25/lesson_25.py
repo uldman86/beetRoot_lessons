b9b3c3e772037706f5f9d8d9d694cf8df958d76a
Sergey <uldman.sergey@gmail.com>
2022-02-16 19:04:50 +0200

    Add lesson 25 Task 5

--- lesson_25/lesson_25.py
+++ lesson_25/lesson_25.py
@@ -132,3 +132,26 @@ if __name__ == '__main__':
     assert reverse('o') == 'o'
 
 
+# Task 5
+#
+# def sum_of_digits(digit_string: str) -> int:
+#     """
+#     >>> sum_of_digits('26') == 8
+#     True
+#
+#     >>> sum_of_digits('test')
+#     ValueError("input string must be digit string")
+#     """
+
+def sum_of_digits(digit_string: str) -> int:
+    if not digit_string:
+        raise ValueError('string empty')
+    if not digit_string.isdigit():
+        raise ValueError("input string must be digit string")
+    if len(digit_string) == 1:
+        return int(digit_string)
+
+    return int(digit_string[0]) + sum_of_digits(digit_string[1:])
+
+assert sum_of_digits('26') == 8
+#sum_of_digits('test')
